c25dec7ece345697bcecbed03ee6e829866579f0
janakhaled1 <janakhaledsh@gmail.com>
2026-06-16 23:21:32 +0300

    Remove hardcoded AWS credentials from upload_outputs.py
    Removed hardcoded AWS credentials and region.

--- match_prediction/upload_outputs.py
+++ match_prediction/upload_outputs.py
@@ -5,9 +5,9 @@ from decimal import Decimal
 
 dynamodb = boto3.resource(
     'dynamodb',
-    region_name='eu-north-1',
-    aws_access_key_id='***REMOVED***',
-    aws_secret_access_key='***REMOVED***'
+    region_name='',
+    aws_access_key_id='',
+    aws_secret_access_key=''
 )
 
 def to_decimal(obj):
